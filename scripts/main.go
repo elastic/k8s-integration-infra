23667d2e8ee697f40252d57f6820e48ef2e611dd
MichaelKatsoulis <michaelkatsoulis88@gmail.com>
2022-03-03 15:30:16 +0200

    Update code

--- scripts/main.go
+++ scripts/main.go
@@ -8,7 +8,6 @@ import (
 
 	appsv1 "k8s.io/api/apps/v1"
 	apiv1 "k8s.io/api/core/v1"
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
@@ -16,6 +15,42 @@ import (
 	"k8s.io/client-go/util/homedir"
 	"k8s.io/client-go/util/retry"
 )
+
+var deployment = &appsv1.Deployment{
+	ObjectMeta: metav1.ObjectMeta{
+		Name: "demo-deployment",
+	},
+	Spec: appsv1.DeploymentSpec{
+		Replicas: int32Ptr(int32(1)),
+		Selector: &metav1.LabelSelector{
+			MatchLabels: map[string]string{
+				"app": "demo",
+			},
+		},
+		Template: apiv1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: map[string]string{
+					"app": "demo",
+				},
+			},
+			Spec: apiv1.PodSpec{
+				Containers: []apiv1.Container{
+					{
+						Name:  "web",
+						Image: "nginx:1.12",
+						Ports: []apiv1.ContainerPort{
+							{
+								Name:          "http",
+								Protocol:      apiv1.ProtocolTCP,
+								ContainerPort: 80,
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
 
 func main() {
 	var kubeconfig *string
@@ -37,89 +72,73 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
-	deployment := &appsv1.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "demo-deployment",
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: int32Ptr(int32(*replicas)),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{
-					"app": "demo",
-				},
-			},
-			Template: apiv1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: map[string]string{
-						"app": "demo",
-					},
-				},
-				Spec: apiv1.PodSpec{
-					Containers: []apiv1.Container{
-						{
-							Name:  "web",
-							Image: "nginx:1.12",
-							Ports: []apiv1.ContainerPort{
-								{
-									Name:          "http",
-									Protocol:      apiv1.ProtocolTCP,
-									ContainerPort: 80,
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-	}
 
-	nsName := "demo"
+	// Create namespaces and deployment per namespace
 	for i := 1; i <= *namespaces; i++ {
-		Nname := fmt.Sprintf("%s-%d", nsName, i)
-		Dname := fmt.Sprintf("demo-deployment-%d", i)
-		nsSpec := &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: Nname}}
-		nsClient := client.CoreV1().Namespaces()
-
-		_, getErr := nsClient.Get(context.TODO(), Nname, metav1.GetOptions{})
-		if getErr != nil {
-			fmt.Printf("Namespace %s does not exist\n", Nname)
-			_, err = nsClient.Create(context.TODO(), nsSpec, metav1.CreateOptions{})
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println("Namespace created")
+		Nname := fmt.Sprintf("demo-%d", i)
+		err := createNamespace(Nname, client)
+		if err != nil {
+			panic(err)
 		}
-		fmt.Printf("Namespace %s exists\n", Nname)
-		deploymentsClient := client.AppsV1().Deployments(Nname)
 
-		result, getErr := deploymentsClient.Get(context.TODO(), Dname, metav1.GetOptions{})
-		if getErr != nil {
-			fmt.Printf("Deployment %s does not exist", Dname)
-			// Create Deployment
-			fmt.Println("Creating deployment...")
-			deployment.Name = Dname
-			result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
-			if err != nil {
-				panic(err)
-			}
-			fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
-		} else {
-			fmt.Printf("Deployment %s exists\n", Dname)
-			retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-				// Retrieve the latest version of Deployment before attempting update
-				// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
-
-				result.Spec.Replicas = int32Ptr(int32(*replicas)) // update replica count
-				_, updateErr := deploymentsClient.Update(context.TODO(), result, metav1.UpdateOptions{})
-				return updateErr
-			})
-			if retryErr != nil {
-				panic(fmt.Errorf("Update failed: %v", retryErr))
-			}
-			fmt.Printf("Updated Deployment %s\n", Dname)
+		Dname := fmt.Sprintf("demo-deployment-%d", i)
+		err = createOrUpdateDeployment(Dname, Nname, client, *replicas)
+		if err != nil {
+			panic(err.Error())
 		}
 	}
 
+}
+
+// createNamespace creates a new namespace if it does not exist
+func createNamespace(name string, client *kubernetes.Clientset) error {
+	nsSpec := &apiv1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: name}}
+	nsClient := client.CoreV1().Namespaces()
+
+	_, getErr := nsClient.Get(context.TODO(), name, metav1.GetOptions{})
+	if getErr != nil {
+		fmt.Printf("Namespace %s does not exist\n", name)
+		_, err := nsClient.Create(context.TODO(), nsSpec, metav1.CreateOptions{})
+		if err != nil {
+			return err
+		}
+		fmt.Println("Namespace created")
+	}
+	fmt.Printf("Namespace %s exists\n", name)
+	return nil
+}
+
+// createOrUpdateDeployment creates a new namespace if it does not exist or updates it ifit does
+func createOrUpdateDeployment(name, namespace string, client *kubernetes.Clientset, replicas int) error {
+	deploymentsClient := client.AppsV1().Deployments(namespace)
+	result, getErr := deploymentsClient.Get(context.TODO(), name, metav1.GetOptions{})
+	if getErr != nil {
+		fmt.Printf("Deployment %s does not exist\n", name)
+		// Create Deployment
+		fmt.Println("Creating deployment...")
+		deployment.Name = name
+		deployment.Spec.Replicas = int32Ptr(int32(replicas))
+		result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
+	} else {
+		fmt.Printf("Deployment %s exists\n", name)
+		retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+			// Retrieve the latest version of Deployment before attempting update
+			// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
+
+			result.Spec.Replicas = int32Ptr(int32(replicas)) // update replica count
+			_, updateErr := deploymentsClient.Update(context.TODO(), result, metav1.UpdateOptions{})
+			return updateErr
+		})
+		if retryErr != nil {
+			return fmt.Errorf("Update failed: %v", retryErr)
+		}
+		fmt.Printf("Updated Deployment %s\n", name)
+	}
+	return nil
 }
 
 func int32Ptr(i int32) *int32 { return &i }
