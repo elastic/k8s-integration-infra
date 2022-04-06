629d311f9d4ec76db29d499e4cb47fa5955edc15
Giuseppe Santoro <giuseppe.santoro@elastic.co>
2022-04-06 16:06:40 +0100

    fix namespace in metricbeat and filebeat helm resources. terraform fmt. added tfstate to .gitignore

--- infra/helm.tf
+++ infra/helm.tf
@@ -27,49 +27,49 @@ variable "imageTag" {
 
 
 resource "helm_release" "metricbeat" {
-  count       =  var.deployBeat ? 1 : 0
-  name        = "metricbeat"
-  chart       = "metricbeat"
-  repository  = "./charts"
-  namespace   = "default"
-  max_history = 3
+  count            = var.deployBeat ? 1 : 0
+  name             = "metricbeat"
+  chart            = "metricbeat"
+  repository       = "./charts"
+  namespace        = var.namespace
+  max_history      = 3
   create_namespace = true
   wait             = true
   reset_values     = true
   set {
-      name  = "imageTag"
-      value = var.imageTag
+    name  = "imageTag"
+    value = var.imageTag
   }
 }
 
 resource "helm_release" "filebeat" {
-  count       =  var.deployBeat ? 1 : 0
-  name        = "filebeat"
-  chart       = "filebeat"
-  repository  = "./charts"
-  namespace   = "default"
-  max_history = 3
+  count            = var.deployBeat ? 1 : 0
+  name             = "filebeat"
+  chart            = "filebeat"
+  repository       = "./charts"
+  namespace        = var.namespace
+  max_history      = 3
   create_namespace = true
   wait             = true
   reset_values     = true
   set {
-      name  = "imageTag"
-      value = var.imageTag
+    name  = "imageTag"
+    value = var.imageTag
   }
 }
 
 resource "helm_release" "elastic-agent" {
-  count       =  var.deployAgent ? 1 : 0
-  name        = "elastic-agent"
-  chart       = "elastic-agent"
-  repository  = "./charts"
-  namespace   = var.namespace
-  max_history = 3
+  count            = var.deployAgent ? 1 : 0
+  name             = "elastic-agent"
+  chart            = "elastic-agent"
+  repository       = "./charts"
+  namespace        = var.namespace
+  max_history      = 3
   create_namespace = true
   wait             = true
   reset_values     = true
   set {
-      name  = "imageTag"
-      value = var.imageTag
+    name  = "imageTag"
+    value = var.imageTag
   }
 }
